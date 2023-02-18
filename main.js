5ea719960ae2f8deef623c0be330ae4b26f3b0dd
mmed <mmed.benhadjnasr@gmail.com>
2023-02-18 16:53:36 +0100

    sidebar + responve navbar

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 $(window).scroll(function () {
-    if ($(window).scrollTop()) {
-      $("nav").addClass("black");
-    } else $("nav").removeClass("black");
-  });
+  if ($(window).scrollTop()) {
+    $("nav").addClass("black");
+  } else $("nav").removeClass("black");
+});
+function open_sidebar() {
+  var sideBar=document.getElementById("sidebar");
+  if (sideBar.style.width == "50vw")
+    sideBar.style.width = "0px";
+  else sideBar.style.width = "50vw";
+}
