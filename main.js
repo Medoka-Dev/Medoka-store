dca5b1d9d43c42d3cbed925514d42bafa656c84d
mmed <mmed.benhadjnasr@gmail.com>
2023-02-24 00:19:33 +0100

    i have no idea what i did

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-// this function open the side bar
+//* copy the content of the navbar in the sidebar
+$( document ).ready(function() {
+  document.getElementById("sidebar-list").innerHTML = document.getElementById("navlist").innerHTML;
+  document.getElementById("sidebar-user-info").innerHTML = document.getElementById("user-info").innerHTML;
+});
+//* this function open the side bar
 function open_sidebar() {
   var sideBar = document.getElementById("sidebar");
-  if (sideBar.style.width == "50vw") sideBar.style.width = "0px";
-  else sideBar.style.width = "50vw";
+  if (sideBar.style.width == "60vw") sideBar.style.width = "0px";
+  else sideBar.style.width = "60vw";
 }
 function close_sidebar() {
   var sideBar = document.getElementById("sidebar");
   if (screen.width > 750) sideBar.style.width = "0px";
 }
-// this make the nav bar dissappear on scroll down and reappear on scroll up
-// this fuction is disabaled if the side bar is open
+//* this make the nav bar dissappear on scroll down and reappear on scroll up
+//* this fuction is disabaled if the side bar is open
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (currentScrollPos < 400) {
     document.getElementById("navbar").style.top = "0";
-    document.getElementById("menu-icon").style.top = "35px";
     return;
   }
-  if (!(document.getElementById("sidebar").style.width == "50vw")) {
+  if (!(document.getElementById("sidebar").style.width == "60vw")) {
     if (prevScrollpos < currentScrollPos) {
       document.getElementById("navbar").style.top = "-70px";
-      document.getElementById("menu-icon").style.top = "-35px";
     } else {
       document.getElementById("navbar").style.top = "0";
-      document.getElementById("menu-icon").style.top = "35px";
     }
   }
   prevScrollpos = currentScrollPos;
