f5f3987c493ebb6448eb3d72226bc8b2545584f4
mmed-hajnasr <123130947+mmed-hajnasr@users.noreply.github.com>
2023-04-23 20:51:35 +0100

    Add files via upload

--- src/login.php
+++ src/login.php
@@ -9,9 +9,10 @@ if (isset($_POST["submit"])) {
     $password = $_POST["login-pass"];
     $account = get_account($conn, $email, $password);
     if ($account == null) {
-        // header("location: ../index.php?err=nouser");
+        header("location: ../index.php?err=nouser");
     } else {
         $_SESSION["in"] = true;
+        $_SESSION["uid"]=$account["user_id"];
         $_SESSION["username"] = $account["username"];
         if ($_POST["submit"] == "Get Started!!") {
             header("location: ../index.php?err=in");
