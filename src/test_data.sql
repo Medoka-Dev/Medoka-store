8c72b57161bb74512964474f49f288b054a001f3
mmed <mmed.benhadjnasr@gmail.com>
2023-04-23 02:34:42 +0100

    FIXED THE PRICES

--- src/test_data.sql
+++ src/test_data.sql
@@ -32,7 +32,7 @@ VALUES (
         'resident Evil 4.jpg',
         'CAPCOM',
         'Survival is just the beginning. Six years have passed since the biological disaster in Raccoon City. Leon S. Kennedy, one of the survivors, tracks the president’s kidnapped daughter to a secluded European village, where there is something terribly wrong with the locals.',
-        24
+        179
     );
 INSERT INTO games (
         label,
@@ -50,7 +50,7 @@ VALUES (
         'hogwarts.jpg',
         'Warner Bros. Games',
         'Hogwarts Legacy is an immersive, open-world action RPG. Now you can take control of the action and be at the center of your own adventure in the wizarding world.',
-        34
+        179
     );
 INSERT INTO games (
         label,
@@ -68,7 +68,7 @@ VALUES (
         'the last of us.jpg',
         'Sony Interactive Entertainment',
         'Le survival action The Last of Us sur PS3 suit Joel et d’Ellie à travers les Etats-Unis. Les deux devront s’entraider pour survivre à une mystérieuse peste. La nature commence à s’approprier les villes abandonnées et les quelques survivants s’entretuent pour récupérer le peu de nourriture et d’armes encore présentes.',
-        34
+        189
     );
 INSERT INTO games (
         label,
@@ -86,7 +86,7 @@ VALUES (
         'The last of us 2.jpg',
         'Sony Interactive Entertainment',
         'Five years after their dangerous journey across the post-pandemic United States, Ellie and Joel have settled down in Jackson, Wyoming. Living amongst a thriving community of survivors has allowed them peace and stability, despite the constant threat of the infected and other, more desperate survivors. When a violent event disrupts that peace, Ellie embarks on a relentless journey to carry out justice and find closure. As she hunts those responsible one by one, she is confronted with the devastating physical and emotional repercussions of her actions.',
-        24
+        149
     );
 INSERT INTO games (
         label,
@@ -104,7 +104,7 @@ VALUES (
         'overcooked 2.jpg',
         'Team17',
         'Overcooked returns with a brand-new helping of chaotic cooking action! Journey back to the Onion Kingdom and assemble your team of chefs in classic couch co-op or online play for up to four players. Hold onto your aprons … it s time to save the world (again!)Out of the frying pan, into the fire… You ve saved the world from the Ever Peckish. Now a new threat has arisen and it’s time to get back in the kitchen to stave off the hunger of The Unbread!',
-        42
+        74
     );
 INSERT INTO games (
         label,
@@ -122,7 +122,7 @@ VALUES (
         'the witcher3.jpg',
         'CD PROJEKT RED',
         'The Witcher Wild Hunt is a story-driven, next-generation open world role-playing game set in a visually stunning fantasy universe full of meaningful choices and impactful consequences.',
-        34
+        45
     );
 INSERT INTO games (
         label,
@@ -140,7 +140,7 @@ VALUES (
         'uncharted 4.jpg',
         ' PlayStation PC LLC ',
         'Play as Nathan Drake and Chloe Frazer in their own standalone adventures as they confront their pasts and forge their own legacies. This game includes the critically acclaimed single-player stories from both UNCHARTED 4: A Thief’s End and UNCHARTED: The Lost Legacy.',
-        25
+        149
     );
 INSERT INTO games (
         label,
@@ -158,7 +158,7 @@ VALUES (
         'stardew valley.jpg',
         'ConcernedApe',
         'You ve inherited your grandfather s old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life. Can you learn to live off the land and turn these overgrown fields into a thriving home?',
-        45
+        74
     );
 INSERT INTO games (
         label,
@@ -176,7 +176,7 @@ VALUES (
         'rdr2.jpg',
         'Rockstar Games',
         'Winner of over 175 Game of the Year Awards and recipient of over 250 perfect scores, RDR2 is the epic tale of outlaw Arthur Morgan and the infamous Van der Linde gang, on the run across America at the dawn of the modern age. Also includes access to the shared living world of Red Dead Online.',
-        35
+        59
     );
 INSERT INTO games (
         label,
@@ -194,7 +194,7 @@ VALUES (
         'Valheim.jpg',
         'Coffee Stain Publishing',
         'A brutal exploration and survival game for 1-10 players, set in a procedurally-generated purgatory inspired by viking culture. Battle, build, and conquer your way to a saga worthy of Odin’s patronage!',
-        35
+        49
     );
 INSERT INTO games (
         label,
@@ -212,5 +212,5 @@ VALUES (
         'Outlast 2.jpg',
         'Red Barrels',
         'Outlast 2 introduces you to Sullivan Knoth and his followers, who left our wicked world behind to give birth to Temple Gate, a town, deep in the wilderness and hidden from civilization. Knoth and his flock are preparing for the tribulations of the end of times and you’re right in the thick of it.',
-        12
+        89
     );
